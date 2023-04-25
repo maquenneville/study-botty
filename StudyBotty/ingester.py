6ab417fda1359ba7bef93bb14d0868972fa98eb7
Marc Quenneville <100183934+maquenneville@users.noreply.github.com>
2023-04-24 19:02:27 -0700

    Add files via upload

---
+++ StudyBotty/ingester.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 22 23:21:38 2023
+
+@author: marca
+"""
+
+
+import docx
+import openpyxl
+import pandas as pd
+import chardet
+import io
+from pdfminer.high_level import extract_text
+import os
+import tiktoken
+import os
+from openai_pinecone_tools import generate_response
+
+
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+
+def read_pdf(file_path):
+    try:
+        text = extract_text(file_path)
+    except Exception as e:
+        print(f"Error reading PDF file {file_path}: {str(e)}")
+        text = ""
+    return text
+
+def read_docx(file_path):
+    try:
+        doc = docx.Document(file_path)
+        text = " ".join([p.text for p in doc.paragraphs])
+    except Exception as e:
+        print(f"Error reading DOCX file {file_path}: {str(e)}")
+        text = ""
+    return text
+
+
+
+def read_xlsx_file(file_path):
+    try:
+        df = pd.read_excel(file_path)
+    except Exception as e:
+        print(f"Error reading XLSX file {file_path}: {str(e)}")
+        df = None
+        
+    file_name = os.path.basename(file_path)
+    return file_name, df
+
+def read_csv_file(file_path):
+    try:
+        with open(file_path, "rb") as f:
+            encoding = chardet.detect(f.read())["encoding"]
+        df = pd.read_csv(file_path, encoding=encoding)
+    except Exception as e:
+        print(f"Error reading CSV file {file_path}: {str(e)}")
+        df = None
+    
+    file_name = os.path.basename(file_path)
+    return file_name, df
+
+
+def read_txt(file_path):
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            text = f.read()
+    except Exception as e:
+        print(f"Error reading TXT file {file_path}: {str(e)}")
+        text = ""
+    return text
+
+
+
+def csv_id_agent(context):
+    
+    if len(context) > 2000:
+    
+        context = context[:2000]
+    
+    messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "You are my CSV Indentification Assistant. Your job is to take a block of text provided below, and decide if the text represents all or part of a comma separated value text.  You must decide, and must answer using either 'yes' or 'no'."},
+            {"role": "user", "content": f"Text to be identified: {context}"}
+        ]
+    
+    response = generate_response(
+        messages, temperature=0.0, n=1, max_tokens=10, frequency_penalty=0
+    )
+    is_csv = None
+    
+    if "yes" in response.lower():
+        is_csv = True
+    elif "no" in response.lower():
+        is_csv = False
+        
+    else:
+        print("I can't tell is this is a CSV, I'm sorry!")
+        return
+    
+    return is_csv
+
+
+
+
+def process_table_file(file_path):
+    def count_tokens(text):
+        tokens = len(encoding.encode(text))
+        return tokens
+
+    file_ext = os.path.splitext(file_path)[1].lower()
+
+    if file_ext == '.csv':
+        file_name, df = read_csv_file(file_path)
+    elif file_ext == '.xlsx':
+        file_name, df = read_xlsx_file(file_path)
+    else:
+        raise ValueError("Unsupported file extension. Please provide a CSV or XLSX file.")
+
+    if df is None:
+        return None
+
+    plain_csv_text = df.to_csv(index=False)
+    rows = plain_csv_text.split("\n")
+    
+    chunk_tokens = 0
+    chunks = []
+    chunk = ""
+
+    for row in rows:
+        row_tokens = len(encoding.encode(row))
+        chunk_tokens += row_tokens
+
+        if chunk_tokens >= 1000:
+            chunks.append(chunk)
+            chunk = ""
+            chunk_tokens = row_tokens
+
+        chunk += "\n" + row
+
+    # Add the remaining chunk, if any
+    if chunk:
+        chunks.append(chunk)
+
+    chunks = [f"{file_name}\n{chunk}" for chunk in chunks]
+    
+    return chunks
+
+
+
+
+
+
+def ingester(file_path):
+    extension = file_path.split(".")[-1].lower()
+    if extension == "pdf":
+        return read_pdf(file_path)
+    elif extension in ["doc", "docx"]:
+        return read_docx(file_path)
+    elif extension in ["csv", "xlsx"]:
+        return process_table_file(file_path)
+    elif extension == "txt":
+        return read_txt(file_path)
+    else:
+        print(f"Unsupported file type: {extension}")
+        return ""
+    
+
+
+def chunk_text(text, chunk_size=1000):
+    words = text.split()
+    chunks = []
+    current_chunk = ""
+
+    for word in words:
+        
+        # Check if adding the word to the current chunk would exceed the chunk size
+        if len(current_chunk) + len(word) + 1 > chunk_size:
+            # If so, add the current chunk to the chunks list and start a new chunk with the current word
+            chunks.append(current_chunk.strip())
+            current_chunk = word
+        else:
+            # Otherwise, add the word to the current chunk
+            current_chunk += f" {word}"
+
+    # Add the last chunk to the chunks list
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+    
+
+    return chunks
+
+def ingest_folder(folder_path, progress=True):
+    context_chunks = []
+
+    # List all files in the folder
+    file_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    total_files = len(file_paths)
+
+    for i, file_path in enumerate(file_paths):
+        if progress:
+            print(f"Processing {file_path}")
+
+        text = ingester(file_path)
+
+        if isinstance(text, str):
+            chunks = chunk_text(text)
+            context_chunks.extend(chunks)
+        
+        else:
+            context_chunks.extend(text)
+
+    return context_chunks
+
+#print(ingest_folder(r"C:\Users\marca\Desktop\Coding\ChatGPT\embeddings\DocBot\test_input"))
+#print(process_table_file(r"C:\Users\marca\Desktop\Coding\ChatGPT\embeddings\AutoTutor\test_input\Raw Mussel data 2017 (1).xlsx"))
---
+++ StudyBotty/openai_pinecone_tools.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 23 18:31:41 2023
+
+@author: marca
+"""
+
+
+
+import tiktoken
+import configparser
+import openai
+from openai.error import RateLimitError, InvalidRequestError, APIError
+import pinecone
+from pinecone import PineconeProtocolError
+import time
+from tqdm import tqdm
+import pandas as pd
+
+
+
+
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+
+
+def count_tokens(text):
+    tokens = len(encoding.encode(text))
+    return tokens
+
+def get_api_keys(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    openai_api_key = config.get("API_KEYS", "OpenAI_API_KEY")
+    pinecone_api_key = config.get("API_KEYS", "Pinecone_API_KEY")
+    pinecone_env = config.get("API_KEYS", "Pinecone_ENV")
+    index = config.get("API_KEYS", "Pinecone_Index")
+    namespace = config.get("API_KEYS", "Namespace")
+
+
+    return openai_api_key, pinecone_api_key, pinecone_env, index, namespace
+
+openai_api_key, pinecone_api_key, pinecone_env, index, namespace = get_api_keys('config.ini')
+
+openai.api_key = openai_api_key
+
+
+CHAT_MODEL = "gpt-3.5-turbo"
+EMBEDDING_MODEL = "text-embedding-ada-002"
+PINECONE_INDEX = index
+PINECONE_NAMESPACE = namespace
+PINECONE_API_KEY = pinecone_api_key
+PINECONE_ENV = pinecone_env
+
+
+
+def get_embedding(text: str, model: str=EMBEDDING_MODEL):
+    while True:
+        try:
+            result = openai.Embedding.create(
+              model=model,
+              input=text
+            )
+            break
+        except (APIError, RateLimitError):
+            print("OpenAI had an issue, trying again in a few seconds...")
+            time.sleep(10)
+    return result["data"][0]["embedding"]
+
+
+
+def create_embeddings_dataframe(context_chunks):
+
+
+    # Calculate embeddings for each chunk with a progress bar
+    embeddings = []
+    for chunk in tqdm(context_chunks, desc="Calculating embeddings"):
+        embedding = get_embedding(chunk)
+        embeddings.append(embedding)
+
+    # Create the DataFrame with index and chunk columns
+    df = pd.DataFrame({"index": range(len(context_chunks)), "chunk": context_chunks})
+
+    # Add the embeddings to the DataFrame in separate columns with the naming convention "embedding{num}"
+    embeddings_df = pd.DataFrame(embeddings, columns=[f"embedding{i}" for i in range(1536)])
+
+    # Concatenate the main DataFrame with the embeddings DataFrame
+    result_df = pd.concat([df, embeddings_df], axis=1)
+
+    return result_df
+
+
+def store_embeddings_in_pinecone(namespace=PINECONE_NAMESPACE, index=PINECONE_INDEX, pinecone_api_key=PINECONE_API_KEY, pinecone_env=PINECONE_ENV, dataframe=None):
+    # Initialize Pinecone
+    pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+
+    # Instantiate Pinecone's Index
+    pinecone_index = pinecone.Index(index_name=index)
+
+    if dataframe is not None and not dataframe.empty:
+        batch_size = 80
+        vectors_to_upsert = []
+        batch_count = 0
+
+        # Calculate the total number of batches
+        total_batches = -(-len(dataframe) // batch_size)
+
+        # Create a tqdm progress bar object
+        progress_bar = tqdm(total=total_batches, desc="Loading info into Pinecone")
+
+        for index, row in dataframe.iterrows():
+            context_chunk = row["chunk"]
+            
+            vector = [float(row[f"embedding{i}"]) for i in range(1536)]
+            
+            pine_index = f"hw_{index}"
+            metadata = {"context": context_chunk}
+            vectors_to_upsert.append((pine_index, vector, metadata))
+
+            # Upsert when the batch is full or it's the last row
+            if len(vectors_to_upsert) == batch_size or index == len(dataframe) - 1:
+                while True:
+                     
+                    try:
+                        upsert_response = pinecone_index.upsert(
+                            vectors=vectors_to_upsert,
+                            namespace=namespace
+                        )
+
+                        batch_count += 1
+                        vectors_to_upsert = []
+
+                        # Update the progress bar
+                        progress_bar.update(1)
+                        break
+
+                    except pinecone.core.client.exceptions.ApiException:
+                        print("Pinecone is a little overwhelmed, trying again in a few seconds...")
+                        time.sleep(10)
+
+        # Close the progress bar after completing all upserts
+        progress_bar.close()
+
+    else:
+        print("No dataframe to retrieve embeddings")
+        
+        
+        
+
+def fetch_context_from_pinecone(query, top_n=5, index=PINECONE_INDEX, namespace=PINECONE_NAMESPACE, pinecone_api_key=PINECONE_API_KEY, pinecone_env=PINECONE_ENV):
+    # Initialize Pinecone
+    pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+
+    # Generate the query embedding
+    query_embedding = get_embedding(query)
+
+    # Query Pinecone for the most similar embeddings
+    pinecone_index = pinecone.Index(index_name=index)
+    
+    
+    while True:
+        try:
+            query_response = pinecone_index.query(
+                namespace=namespace,
+                top_k=top_n,
+                include_values=False,
+                include_metadata=True,
+                vector=query_embedding
+                
+            )
+            break
+        
+        except PineconeProtocolError:
+            print("Pinecone needs a moment....")
+            time.sleep(3)
+            continue
+    
+    # Retrieve metadata for the relevant embeddings
+    context_chunks = [match['metadata']['context'] for match in query_response['matches']]
+    
+    #context = "\n".join(context_chunks)
+
+    return context_chunks
+
+
+
+
+def generate_response(
+    messages, model_engine="gpt-3.5-turbo", temperature=0.5, n=1, max_tokens=4000, frequency_penalty=0
+):
+
+    
+
+    # Calculate the number of tokens in the messages
+    tokens_used = sum([count_tokens(msg["content"]) for msg in messages])
+    tokens_available = 4096 - tokens_used
+
+    # Adjust max_tokens to not exceed the available tokens
+    max_tokens = min(max_tokens, (tokens_available - 100))
+
+    # Reduce max_tokens further if the total tokens exceed the model limit
+    if tokens_used + max_tokens > 4096:
+        max_tokens = 4096 - tokens_used - 10
+
+    if max_tokens < 1:
+        max_tokens = 1
+
+    # Generate a response
+    max_retries = 10
+    retries = 0
+    while True:
+        if retries < max_retries:
+            try:
+                completion = openai.ChatCompletion.create(
+                    model=model_engine,
+                    messages=messages,
+                    n=n,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    frequency_penalty=frequency_penalty,
+                )
+                break
+            except (RateLimitError, KeyboardInterrupt):
+                time.sleep(60)
+                retries += 1
+                print("Server overloaded, retrying in a minute")
+                continue
+        else:
+            print("Failed to generate prompt after max retries")
+            return
+    response = completion.choices[0].message.content
+    return response
+
+
+#print(fetch_context_from_pinecone("What are the columns in the Raw Mussel data 2017 (1) table?"))
